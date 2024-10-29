d268df3bc7b687743a99e864d30b1059392ebbe4
TJ <thaddeusjiang@gmail.com>
2024-10-29 11:18:12 +0900

    refactor: 💡 get_env

--- lib/small_sdk/telegram.ex
+++ lib/small_sdk/telegram.ex
@@ -6,7 +6,8 @@ defmodule SmallSdk.Telegram do
   plug(Tesla.Middleware.BaseUrl, "https://api.telegram.org")
 
   def download_file_content(file_path) when is_binary(file_path) do
-    url = "/file/bot#{Application.fetch_env!(:save_it, :telegram_bot_token)}/#{file_path}"
+    bot_token = get_env()
+    url = "/file/bot#{bot_token}/#{file_path}"
 
     case get(url) do
       {:ok, response} ->
@@ -22,5 +23,11 @@ defmodule SmallSdk.Telegram do
       {:ok, body} -> body
       {:error, error} -> raise "Error: #{inspect(error)}"
     end
+  end
+
+  defp get_env() do
+    bot_token = Application.fetch_env!(:save_it, :telegram_bot_token)
+
+    bot_token
   end
 end
