69103cbce999e4d3a24f80bb74f1f2fd3e22260b
天海 原 <thaddeusjiang@gmail.com>
2026-04-26 21:37:09 +0900

    bump 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SaveIt.MixProject do
   def project do
     [
       app: :save_it,
-      version: "0.2.0-rc.1",
+      version: "0.2.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
