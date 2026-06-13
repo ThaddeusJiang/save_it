c0dd02e3b14c5c82602bbf599617a7f53e819fa8
天海 原 <thaddeusjiang@gmail.com>
2026-06-13 21:46:39 +0900

    fix: simplify test config import

--- config/config.exs
+++ config/config.exs
@@ -13,8 +13,6 @@ config :logger, :default_formatter,
     error: :red
   ]
 
-env_config = Path.expand("#{config_env()}.exs", __DIR__)
-
-if File.exists?(env_config) do
-  import_config "#{config_env()}.exs"
+if config_env() == :test do
+  import_config "test.exs"
 end
